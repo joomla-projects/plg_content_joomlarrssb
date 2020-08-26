54f5df9d81ff0a3343e354730ad5c39e6c3c89d2
zero-24 <zero-24@users.noreply.github.com>
2020-08-26 22:12:41 +0200

    fix build script and git ignore

--- build/build.sh
+++ build/build.sh
@@ -3,7 +3,7 @@ EXTENSION_ZIP_FILENAME="build/plg_content_joomlarrssb.zip"
 EXTENSION_ELEMENT="joomlarrssb"
 if [ ! -f "$EXTENSION_ELEMENT.xml" ]; then cd ..; fi
 if [ -f "$EXTENSION_ZIP_FILENAME" ]; then rm $EXTENSION_ZIP_FILENAME; fi
-zip -r $EXTENSION_ZIP_FILENAME language/ "$EXTENSION_ELEMENT.php" "$EXTENSION_ELEMENT.xml" script.php --quiet
+zip -r $EXTENSION_ZIP_FILENAME language/ media/ tmpl/ "$EXTENSION_ELEMENT.php" "$EXTENSION_ELEMENT.xml" script.php --quiet
 SHA512=$(sha512sum $EXTENSION_ZIP_FILENAME | awk '{print $1}')
 sed -i -e "s/\(<sha512>\).*\(<\/sha512>\)/<sha512>$SHA512<\/sha512>/g" manifest.xml
 echo 'package and update server ready'
