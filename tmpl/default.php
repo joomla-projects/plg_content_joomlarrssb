aacccac90821d02382363f0d9e9fa642f962f24e
zero-24 <zero-24@users.noreply.github.com>
2020-08-30 20:08:40 +0200

    use the modern style for HTMLHelper options

--- tmpl/default.php
+++ tmpl/default.php
@@ -14,14 +14,15 @@ use Joomla\CMS\HTML\HTMLHelper;
 
 // Import media
 HTMLHelper::_('jquery.framework');
-HTMLHelper::_('script', 'joomlarrssb/rrssb.min.js', false, true);
-HTMLHelper::_('stylesheet', 'joomlarrssb/rrssb.css', [], true);
-HTMLHelper::_('stylesheet', 'joomlarrssb/joomla.css', [], true);
+HTMLHelper::_('script', 'joomlarrssb/rrssb.min.js', ['relative' => true]);
+HTMLHelper::_('stylesheet', 'joomlarrssb/rrssb.css', ['relative' => true]);
+HTMLHelper::_('stylesheet', 'joomlarrssb/joomla.css', ['relative' => true]);
+
 
 // Optional RTL support
 if ($this->app->getDocument()->getDirection() == 'rtl')
 {
-	HTMLHelper::_('stylesheet', 'joomlarrssb/joomla-rtl.css', [], true);
+	HTMLHelper::_('stylesheet', 'joomlarrssb/joomla-rtl.css', ['relative' => true]);
 }
 
 // If the article has an image use it otherwise default is hardcoded to the card image used for Twitter
